122e679bdc578fd780d27db55bb162ca5c37bdc9
SuYJ <1697895745@qq.com>
2018-02-03 18:52:00 +0800

    首页静态

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+;require(['config'],function(){
+    require(['jquery','head','foot','common','carousel'],function(){
+        $('.Yheader').load('../html/head.html');
+        $('.Yfooter').load('../html/foot.html');
+        $('.Ybanner').yjCarousel({
+            imgs:['../images/index/banner1.jpg','../images/index/banner2.jpg','../images/index/banner3.jpg','../images/index/banner4.jpg','../images/index/banner5.jpg'],
+            buttons:true,
+            width:1423,
+            height:500
+        });
+    });
+});
