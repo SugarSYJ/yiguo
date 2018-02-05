c64ce67e6c050f78167c17b3d2ed9c31f2af2f56
SuYJ <1697895745@qq.com>
2018-02-05 21:39:25 +0800

    购物车部分

--- src/js/car.js
+++ src/js/car.js
@@ -1,7 +1,12 @@
 ;require(['config'],function(){
     require(['jquery','head','foot','common'],function(){
-        $('.Yheader').load('../html/head.html');
-        $('.Yfooter').load('../html/foot.html');
+        $('.Yheader').load('../html/head.html',function(){
+            $('.fl p').first().siblings().hide();
+            $('.Yhead_b').hide();
+        });
+        $('.Yfooter').load('../html/foot.html',function(){
+            $('.Yfoot_service').hide();
+        });
         
     });
 });
